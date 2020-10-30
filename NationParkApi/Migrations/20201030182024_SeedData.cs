d422304cb6304721095db7f04c16646d3078fa84
Grant Eadie <grantleadie@gmail.com>
2020-10-30 11:23:08 -0700

    builing seed data.

---
+++ NationParkApi/Migrations/20201030182024_SeedData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NationalParkApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Reserves",
+                columns: new[] { "ReserveId", "ReserveName" },
+                values: new object[,]
+                {
+                    { 1, "North Cascades National Park" },
+                    { 2, "Mt. Hood NationalForest" },
+                    { 3, "Zion National Park" },
+                    { 4, "Yosemite National Park" },
+                    { 5, "Yellowstone National Park" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "States",
+                columns: new[] { "StateId", "StateName" },
+                values: new object[,]
+                {
+                    { 1, "Washington" },
+                    { 2, "Oregon" },
+                    { 3, "Utah" },
+                    { 4, "California" },
+                    { 5, "Montana" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Reserves",
+                keyColumn: "ReserveId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reserves",
+                keyColumn: "ReserveId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reserves",
+                keyColumn: "ReserveId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reserves",
+                keyColumn: "ReserveId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reserves",
+                keyColumn: "ReserveId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 5);
+        }
+    }
+}
--- NationParkApi/Models/NationalParkApiContext.cs
+++ NationParkApi/Models/NationalParkApiContext.cs
@@ -11,6 +11,32 @@ namespace NationalParkApi.Models
     public virtual DbSet<Reserve> Reserves { get; set; }
     public DbSet<StateReserve> StateReserves { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<State>()
+        .HasData(
+          new State { StateId = 1, StateName = "Washington"},
+          new State { StateId = 2, StateName = "Oregon"},
+          new State { StateId = 3, StateName = "Utah"},
+          new State { StateId = 4, StateName = "California"},
+          new State { StateId = 5, StateName = "Montana"}
+      );
+      builder.Entity<Reserve>()
+        .HasData(
+          new Reserve { ReserveId = 1, ReserveName = "North Cascades National Park"},
+          new Reserve { ReserveId = 2, ReserveName = "Mt. Hood NationalForest"},
+          new Reserve { ReserveId = 3, ReserveName = "Zion National Park"},
+          new Reserve { ReserveId = 4, ReserveName = "Yosemite National Park"},
+          new Reserve { ReserveId = 5, ReserveName = "Yellowstone National Park"}
+      );
+      builder.Entity<StateReserve>()
+        .HasData(
+          new StateReserve { StateReserveId = 1, StateId = 1, ReserveId = },
+          new StateReserve { StateReserveId = 2, StateId = "Mt. Hood NationalForest"},
+          new StateReserve { StateReserveId = 3, StateId = "Zion National Park"},
+          new StateReserve { StateReserveId = 4, StateId = "Yosemite National Park"},
+          new StateReserve { StateReserveId = 5, StateId = "Yellowstone National Park"}
+      );
+    }
   }
-
 }
