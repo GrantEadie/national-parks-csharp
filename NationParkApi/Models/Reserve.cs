6bd5e4db7a891b0ac1206eb80a9d0cdf7f8b2f0a
Grant Eadie <grantleadie@gmail.com>
2020-10-30 10:23:27 -0700

    finalize startup and program cs files.

---
+++ NationParkApi/Models/Reserve.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace NationalParkApi.Models
+{
+  public class Reserve
+  {
+    public Reserve()
+    {      
+      this.States = new HashSet<ReserveState>();
+    }
+    public int ReserveId { get; set; }
+    [Required]
+    public string ReserveName { get; set; }
+    public virtual ICollection<ReserveState> States { get; set; }
+    
+  }
+}
