c31018e43df86009d5cda2ca7e42c1b0a02c300d
Grant Eadie <grantleadie@gmail.com>
2020-10-30 10:53:40 -0700

    migrate successfully.

---
+++ NationParkApi/Migrations/20201030175314_Initial.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NationalParkApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reserves",
+                columns: table => new
+                {
+                    ReserveId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ReserveName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reserves", x => x.ReserveId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "States",
+                columns: table => new
+                {
+                    StateId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StateName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_States", x => x.StateId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StateReserves",
+                columns: table => new
+                {
+                    StateReserveId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StateId = table.Column<int>(nullable: false),
+                    ReserveId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StateReserves", x => x.StateReserveId);
+                    table.ForeignKey(
+                        name: "FK_StateReserves_Reserves_ReserveId",
+                        column: x => x.ReserveId,
+                        principalTable: "Reserves",
+                        principalColumn: "ReserveId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StateReserves_States_StateId",
+                        column: x => x.StateId,
+                        principalTable: "States",
+                        principalColumn: "StateId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StateReserves_ReserveId",
+                table: "StateReserves",
+                column: "ReserveId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StateReserves_StateId",
+                table: "StateReserves",
+                column: "StateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StateReserves");
+
+            migrationBuilder.DropTable(
+                name: "Reserves");
+
+            migrationBuilder.DropTable(
+                name: "States");
+        }
+    }
+}
