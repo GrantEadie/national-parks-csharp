29e90216599a86e979d132958535edfd3ab0c547
Grant Eadie <grantleadie@gmail.com>
2020-10-30 11:27:31 -0700

    finalize migration with seed data.

---
+++ NationParkApi/Migrations/20201030182632_SeedData2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NationalParkApi.Migrations
+{
+    public partial class SeedData2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "StateReserves",
+                columns: new[] { "StateReserveId", "ReserveId", "StateId" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 2, 2 },
+                    { 3, 3, 3 },
+                    { 4, 4, 4 },
+                    { 5, 5, 5 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "StateReserves",
+                keyColumn: "StateReserveId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "StateReserves",
+                keyColumn: "StateReserveId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StateReserves",
+                keyColumn: "StateReserveId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "StateReserves",
+                keyColumn: "StateReserveId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "StateReserves",
+                keyColumn: "StateReserveId",
+                keyValue: 5);
+        }
+    }
+}
